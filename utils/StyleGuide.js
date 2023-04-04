8f5eb4877199ab86c481c44059047a5736d1e82d
liatnr <liliwnardi@gmail.com>
2023-04-05 00:31:43 +0300

    add style guide colors

---
+++ utils/StyleGuide.js
@@ -0,0 +1,14 @@
+export const COLORS = {
+    white: '#fff',
+    light_gray: '#D9D9D9',
+    dark_gray: '#716E6E',
+    black: '#000',
+    green: 'rgba(102, 191, 61, 0.7)',
+    red: 'rgba(212, 57, 57, 0.7)',
+    orange: 'rgba(236, 170, 43, 0.7)',
+    blue: '#0047FF',
+    light_blue: '#64C8DE',
+    dark_blue: '#225D7E',
+    pink: '#EBE1FB',
+    yellow: 'rgba(252, 246, 108, 0.89)',
+}
