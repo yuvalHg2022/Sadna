9c095cee88c2b3049c28877a76dd3b46aebaa25c
liatnr <liliwnardi@gmail.com>
2023-04-05 14:13:37 +0300

    fix footer size

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,13 @@
-import { View, Text, Image, StyleSheet } from "react-native";
+import { View, Image, StyleSheet, Dimensions } from "react-native";
 import React from "react";
+import { COLORS } from "../utils/StyleGuide";
+
+const SCREEN_HEIGHT = Dimensions.get("screen").height
 
 const Footer = () => {
   return (
     <View style={styles.container}>
-      <Image source={require("../assets/logo.png")} />
+      <Image source={require("../assets/images/logo.png")} style={{ height: SCREEN_HEIGHT * 0.1 }} resizeMode={'contain'} />
     </View>
   );
 };
@@ -13,9 +16,9 @@ export default Footer;
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "white",
+    backgroundColor: COLORS.white,
     width: "100%",
     alignItems: "center",
-    paddingBottom: 10,
+    paddingBottom: 16,
   },
 });
